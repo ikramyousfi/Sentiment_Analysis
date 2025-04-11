24874fe4c03c3e30df6a2d6bf8985a965a666e86
ikramyousfi <io.yousfi@esi-sba.dz>
2025-04-11 14:32:15 +0200

    small changes

--- app.py
+++ app.py
@@ -7,44 +7,39 @@ from sklearn.feature_extraction.text import CountVectorizer
 import joblib
 import nltk
 
-# Ensure stopwords are downloaded only once
 try:
     stopwords.words('english')
 except LookupError:
     nltk.download('stopwords')
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Load models and vocabulary
 cv = pickle.load(open('vocabulary.pkl', 'rb'))  
 model = joblib.load('Sentiment_Model_trained')
 
-# Define other necessary variables
 stop_words_new = stopwords.words('english')
 stemmer = PorterStemmer()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
-    input_text = ""  # Default empty string for text input
+    input_text = "" 
 
     if request.method == "POST":
-        # Get the input text from the form
         text = request.form['text']
-        input_text = text  # Save the input text for later display
+        input_text = text  
 
-        # Preprocess the input text
+        #clean the input
         text = re.sub('[^a-zA-Z]', ' ', text)
         text = text.lower().split()
         text = [stemmer.stem(word) for word in text if word not in stop_words_new]
         text = ' '.join(text)
+        print(text)
 
-        # Vectorize the text and make the prediction
+        #vectorize the input text and make the prediction
         text_vectorized = cv.transform([text])
         prediction = model.predict(text_vectorized)
 
-        # Display the result
         result = 'Positive' if prediction == 1 else 'Negative'
 
     return render_template("index.html", result=result, input_text=input_text)
